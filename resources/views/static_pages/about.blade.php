6ee866fd89edce4e5fee5dea83d54532996ce9b4
Boris Chen <chenjunchi414@gmail.com>
2016-12-10 17:22:43 +1100

    Finish layout and routes

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.default)
+@extends('layouts.default')
 @section('title', 'About')
 @section('content')
     <h1>About</h1>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.default)
+@extends('layouts.default')
 @section('title', 'Home')
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            THIS IS THE HOME PAGE
+        </p>
+        <P>
+            Everything starts from here.
+        </P>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">Register</a>
+        </p>
+    </div>
 @stop
